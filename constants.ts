60c19daeda5a0aed016ac4fc112a8baa4b3aeaee
Actualización Docente <coord_actualizaciondocente@itdurango.edu.mx>
2025-09-16 15:06:29 -0600

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -1,15 +1,1 @@
-export const LOGO_URL = 'https://githubalex.github.io/web/image.jpg';
-export const DATABASE_URL = 'https://api.allorigins.win/raw?url=https://githubalex.github.io/web/database.xlsx';
 
-export const POTENTIAL_FOLIO_HEADERS = [
-  'folio',
-  'id',
-  'clave',
-  'constancia',
-  'folio de constancia',
-  'folio constancia',
-  'no. folio',
-  'número de folio',
-  'num folio',
-  'folio certificado',
-];
