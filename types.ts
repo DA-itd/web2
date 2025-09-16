e90ea34613e9571b479e5fa831fcb9b224568899
Actualización Docente <coord_actualizaciondocente@itdurango.edu.mx>
2025-09-16 15:08:27 -0600

    Update types.ts

--- types.ts
+++ types.ts
@@ -1,11 +1,1 @@
 
-export enum ValidationStatus {
-  IDLE = 'idle',
-  SUCCESS = 'success',
-  NOT_FOUND = 'not_found',
-  ERROR = 'error',
-}
-
-export interface CertificateData {
-  [key: string]: string | number | null | undefined;
-}
